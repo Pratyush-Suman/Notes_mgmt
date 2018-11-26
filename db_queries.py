7d8fde8b741dc98831366851da48123000eec499
Pratyush-Suman <44948755+Pratyush-Suman@users.noreply.github.com>
2018-11-26 14:20:39 +0530

    Add files via upload

--- db_queries.py
+++ db_queries.py
@@ -11,15 +11,37 @@ def creation():
 	
 	cur.execute("DROP TABLE IF EXISTS student_details;")
 	cur.execute("DROP TABLE IF EXISTS teacher_details;")
+	cur.execute("DROP TABLE IF EXISTS student_notes;")
+	cur.execute("DROP TABLE IF EXISTS uploaded_notes;")
+	cur.execute("DROP VIEW IF EXISTS uplnotelist;")
+	cur.execute("DROP TRIGGER IF EXISTS date_time;")
 
 	cur.execute("CREATE TABLE student_details(SId integer primary key autoincrement default 1001, Name varchar(30),USN varchar(10) unique,Branch char(5),sem integer,section char,Mobile_Number bigint(10),Email_id varchar(40),passwd varchar(20),Login_set char default 'S');")
-	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_Id,passwd) values('Anish R A','4NI16CS001','CSE',5,'A',9012345678,'anishra123@gmail.com','anish123');")
-	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_Id,passwd) values('Vijith K','4NI16ME003','MECH',5,'M',9123456780,'vijithk123@gmail.com','vijith123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Anish R A','4NI16CS001','CSE',5,'A',9012345678,'anishra123@gmail.com','anish123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Vijith K','4NI16ME003','CSE',5,'B',9123456780,'vijithk123@gmail.com','vijith123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('','4NI16','',,'',,'123@gmail.com','123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('','4NI16','',,'',,'123@gmail.com','123');")
 
 
 	cur.execute("CREATE TABLE teacher_details(TId integer primary key autoincrement default 2001,Tname varchar(30),ID_no varchar(11) unique,dept varchar(30),TMobile_Number bigint(10),TEmail_Id varchar(40),tpwd varchar(20),Login_set char default 'T');")
 	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Satya Nadella','5NI2001CS01','CSE',8012345679,'satyanadela123@gmail.com','satya123');")
 	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Sundar Pichai','5NI1998CS03','CSE',8123456790,'sundarpichai123@gmail.com','sundar123');")
+
+	cur.execute("CREATE TABLE student_notes(NId integer primary key autoincrement default 3001, SId integer, Subject varchar(30), Title varchar(30), Notes text ,FOREIGN KEY (SId) REFERENCES student_details(SId));")
+	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(1,'Operating System','Deadlocks','four conditions must hold for there to be a (resource) deadlock: 1. Mutual exclusion condition. 2. Hold-and-wait condition. 3.No-preemption condition. 4.Circular wait condition.');")
+	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(2,'TM','BLAHBLAHBLAHBLAH','BLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAH');")
+ 
+	cur.execute("CREATE TABLE uploaded_notes(UNId integer primary key autoincrement default 4001, TId integer , Subject varchar(30), Title varchar(30), Notes text, Branch char(5), sem integer, section char, Uploaded_On datetime,FOREIGN KEY (TId) REFERENCES teacher_details(TId));")
+	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(1,'DCN','WiMAX','Fixed and Mobile Services','CSE',5,'A');")
+	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(2,'SS','Assembler Directives','RESW BYTE WORD RESW','CSE',5,'B');")
+
+	cur.execute("CREATE VIEW uplnotelist AS SELECT T.Tname,U.Subject,U.Title,U.Notes,U.Branch,U.sem,U.section,U.Uploaded_On from teacher_details T, uploaded_notes U where T.TId=U.TId;")
+
+	cur.execute('''CREATE TRIGGER date_time AFTER INSERT ON uploaded_notes  
+					FOR EACH ROW 
+					BEGIN
+					  UPDATE uploaded_notes set Uploaded_On = datetime('now','localtime') where UNId = new.UNId;
+					END;''')
 
 
 	conn.commit()
@@ -38,7 +60,7 @@ def insert_student_details(Name,USN,Branch,sem,section,Mobile_Number,Email_Id,pa
 		return 0
 
 	else:
-		cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_Id,passwd) values(?, ?, ?, ?, ?, ?, ?,?);", [Name,USN,Branch,sem,section,Mobile_Number,Email_Id,passwd])
+		cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values(?, ?, ?, ?, ?, ?, ?,?);", [Name,USN,Branch,sem,section,Mobile_Number,Email_Id,passwd])
 		conn.commit()
 		conn.close()
 		return 1
@@ -58,3 +80,106 @@ def insert_teacher_details(Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd):
 		cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values(?,?,?,?,?,?);",[Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd])
 		conn.commit()
 		conn.close()
+
+#Determine user type and return the respective home page after login
+
+def verify_user(username,password):
+	
+	conn,cur = connect()
+
+	cur.execute("SELECT * FROM student_details WHERE USN = ? AND passwd = ?;",(username,password))
+
+	data = cur.fetchall()
+
+	if(data):
+		conn.close()
+		return 'S',data
+
+	else:
+		cur.execute("SELECT * FROM teacher_details WHERE ID_no = ? AND tpwd = ?;",(username,password))
+
+		data = cur.fetchall()
+
+		if(data):
+			conn.close()
+			return 'T',data
+
+		else:
+			conn.close()
+			return 0,None
+
+
+def upd_pro_stu(USN,Mobile_Number,Email_Id,passwd):
+	conn,cur = connect()
+
+	cur.execute("UPDATE student_details SET Mobile_Number = ?,Email_id = ?,passwd = ? WHERE USN = ?;",(Mobile_Number,Email_Id,passwd,USN))
+	
+	conn.commit()
+	conn.close()
+
+#Student Add Notes
+def insert_note(stu_ID,Subject,title,stu_note):
+	conn,cur = connect()
+
+	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(?,?,?,?);",[stu_ID,Subject,title,stu_note])
+
+	conn.commit()
+	conn.close()
+
+##Student's Personal Notes List
+def Spernote(SId):
+
+	conn,cur = connect()
+
+	cur.execute("SELECT * from student_notes WHERE SId=?;",[SId])
+
+	Stuuplnote = cur.fetchall()
+
+	conn.close()
+
+	return Stuuplnote
+
+
+##Uploaded notes by teacher in student account
+def Suplnote(Branch,sem,section):
+
+	conn,cur = connect()
+
+	cur.execute("SELECT Tname,Subject,Title,Notes,Uploaded_On FROM uplnotelist WHERE Branch = ? AND sem = ? AND section = ?;",[Branch,sem,section])
+
+	Supnote = cur.fetchall()
+
+	conn.close()
+
+	return Supnote
+
+
+##Teacher Profile Update
+def upd_pro_tea(ID_no,TMobile_Number,TEmail_Id,tpwd):
+	conn,cur = connect()
+
+	cur.execute("UPDATE teacher_details SET TMobile_Number = ?,TEmail_id = ?,tpwd = ? WHERE ID_no = ?;",(TMobile_Number,TEmail_Id,tpwd,ID_no))
+	
+	conn.commit()
+	conn.close()
+
+##Teacher to upload note
+def tea_upl_note(TId,Subject,Title,Notes,Branch,sem,section):
+	conn,cur = connect()
+
+	cur.execute("INSERT INTO  uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(?,?,?,?,?,?,?);", [TId,Subject,Title,Notes,Branch,sem,section])
+
+	conn.commit()
+	conn.close()
+
+##List of uploaded notes by teacher in his account
+def Tuploaded_notes(TId):
+
+	conn,cur = connect()
+
+	cur.execute("SELECT * from uploaded_notes WHERE TId=?;",[TId])
+
+	Teauplnote = cur.fetchall()
+
+	conn.close()
+	return Teauplnote
--- myapp.py
+++ myapp.py
@@ -3,11 +3,26 @@ from db_queries import *
 
 app = Flask(__name__)
 
+user_info = None
+user_info1 = []
+tnote = None
+
 creation()
 
 @app.route('/', methods = ['GET','POST'])
 def landing_page():
 	return render_template('first.html')
+
+@app.route('/first.html', methods = ['GET','POST'])
+def home_page():
+	global user_info ,user_info1
+	user_info = None
+	user_info1 = []
+	return render_template('first.html')
+
+@app.route('/about.html', methods = ['GET','POST'])
+def about_page():
+	return render_template('about.html')
 
 @app.route('/student2.html', methods = ['GET','POST'])
 def student_signup():
@@ -17,12 +32,8 @@ def student_signup():
 def teacher_signup():
 	return render_template('teacher2.html')
 
-@app.route('/login2.html', methods = ['GET','POST'])
-def login_page():
-	return render_template('login2.html')
-
 @app.route("/verify_student", methods = ['POST'])
-def verify_student_info():
+def register_student():
 	Name = request.form['Name']
 	USN = request.form['USN']
 	Branch = request.form['Branch']
@@ -41,7 +52,7 @@ def verify_student_info():
 		return render_template('first.html')
 
 @app.route("/verify_teacher", methods = ['POST'])
-def verify_teacher_info():
+def register_teacher():
 	Tname = request.form['TName']
 	ID_no = request.form['ID_no']
 	dept = request.form['dept']
@@ -56,6 +67,179 @@ def verify_teacher_info():
 
 	elif flag == 1:
 		return render_template('first.html')
+
+
+@app.route('/login2.html', methods = ['GET','POST'])
+def login_page():
+	return render_template('login2.html')
+
+@app.route("/verify_login", methods = ['POST'])
+def verify_login():
+
+	global user_info ,user_info1
+
+	if request.method == 'POST':
+
+		username = request.form['username']
+		password = request.form['password']
+
+		flag,data  = verify_user(username,password)
+
+		if flag == 0:
+			return render_template('login2.html', message = 'WRONG USERNAME OR PASSWORD!!!')
+
+		elif flag == 'S':
+			user_info = data
+			 
+			for i in user_info[0]:
+				user_info1.append(i)
+
+			return render_template('stu_home.html', Name = user_info1[1] )
+
+		elif flag == 'T':
+			user_info = data
+			 
+			for i in user_info[0]:
+				user_info1.append(i)
+
+			return render_template('tea_home.html', Name = user_info1[1] )
+
+@app.route('/stu_home.html',methods = ['GET','POST'])
+def student_homepage():
+	global user_info
+	return render_template('stu_home.html', Name = user_info1[1])
+
+@app.route('/stu_profile.html',methods = ['GET','POST'])
+def student_profilepage():
+	return render_template('stu_profile.html', user_info1 = user_info1)
+
+@app.route('/stu_pro_upd.html',methods = ['GET','POST'])
+def student_proupdpage():
+	return render_template('stu_pro_upd.html')
+
+@app.route('/stu_notes_list.html',methods = ['GET','POST'])
+def student_notelistpage():
+
+	snote = Spernote(user_info1[0])
+
+	return render_template('stu_notes_list.html', snote = snote)
+
+@app.route('/stu_add_note.html',methods = ['GET','POST'])
+def student_addnotepage():
+	return render_template('stu_add_note.html', user_info1 = user_info1)
+
+@app.route('/upl_notes_list.html',methods = ['GET','POST'])
+def student_uplnotelist():
+
+	tsnote = Suplnote(user_info1[3],user_info1[4],user_info1[5])
+
+	return render_template('upl_notes_list.html', tsnote = tsnote)
+
+
+##Student Profile Update
+@app.route('/profile_updated', methods = ['POST'])
+def profile_updated():
+	if request.method == 'POST':
+
+		Mobile_Number = request.form['Mobile_Number']
+		if Mobile_Number == "":
+			Mobile_Number = user_info1[6]
+		Email_Id = request.form['Email_Id']
+		if Email_Id == "":
+			Email_Id = user_info1[7]
+		passwd = request.form['passwd']
+		if passwd == "":
+			passwd = user_info1[8]
+
+		upd_pro_stu(user_info1[2],Mobile_Number,Email_Id,passwd)
+		
+		user_info1[6] = Mobile_Number
+		user_info1[7] = Email_Id
+		user_info1[8] = passwd
+
+		return render_template('stu_profile.html', user_info1 = user_info1)
+
+##Student Adding notes
+@app.route('/add_note', methods = ['POST'])
+def add_note():
+	
+	Subject = request.form['Subject']
+	title = request.form['title']
+	stu_note = request.form['stu_note']
+
+	insert_note(user_info1[0],Subject,title,stu_note) 
+
+	return student_notelistpage()
+
+
+
+@app.route('/tea_home.html',methods = ['GET','POST'])
+def teacher_homepage():
+	return render_template('tea_home.html', Name = user_info1[1])
+
+@app.route('/tea_profile.html',methods = ['GET','POST'])
+def teacher_profilepage():
+	return render_template('tea_profile.html', user_info1 = user_info1)
+
+@app.route('/tea_pro_upd.html',methods = ['GET','POST'])
+def teacher_proupdpage():
+	return render_template('tea_pro_upd.html')
+
+@app.route('/tea_notes_list.html',methods = ['GET','POST'])
+def teacher_notelistpage():
+
+	tnote = Tuploaded_notes(user_info1[0])
+	
+	return render_template('tea_notes_list.html', tnote = tnote)
+
+@app.route('/tea_upl_note.html',methods = ['GET','POST'])
+def teacher_uplnotepage():
+	return render_template('tea_upl_note.html', user_info1 = user_info1)
+
+@app.route('/Tprofile_updated', methods = ['POST'])
+def Tprofile_updated():
+	if request.method == 'POST':
+
+		TMobile_Number = request.form['TMobile_Number']
+		if TMobile_Number == "":
+			TMobile_Number = user_info1[4]
+		TEmail_Id = request.form['TEmail_Id']
+		if TEmail_Id == "":
+			TEmail_Id = user_info1[5]
+		tpwd = request.form['tpwd']
+		if tpwd == "":
+			tpwd = user_info1[6]
+
+		upd_pro_tea(user_info1[2],TMobile_Number,TEmail_Id,tpwd)
+		
+		user_info1[4] = TMobile_Number
+		user_info1[5] = TEmail_Id
+		user_info1[6] = tpwd
+
+		return render_template('tea_profile.html', user_info1 = user_info1)
+
+@app.route('/Note_uploaded', methods = ['POST'])
+def Note_uploaded():
+	if request.method == 'POST':
+
+		Subject = request.form["Subject"]
+		Title = request.form["Title"]
+		Notes = request.form["Notes"]
+		Branch = request.form["Branch"]
+		sem = request.form["sem"]
+		section = request.form["section"]
+
+		tea_upl_note(user_info1[0],Subject,Title,Notes,Branch,sem,section)
+
+		return teacher_notelistpage()
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
