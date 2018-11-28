8d2e88ca569c8056197ca39c872be8a5345c21e4
Pratyush-Suman <44948755+Pratyush-Suman@users.noreply.github.com>
2018-11-28 10:25:03 +0530

    new query

--- db_queries.py
+++ db_queries.py
@@ -16,26 +16,24 @@ def creation():
 	cur.execute("DROP VIEW IF EXISTS uplnotelist;")
 	cur.execute("DROP TRIGGER IF EXISTS date_time;")
 
-	cur.execute("CREATE TABLE student_details(SId integer primary key autoincrement default 1001, Name varchar(30),USN varchar(10) unique,Branch char(5),sem integer,section char,Mobile_Number bigint(10),Email_id varchar(40),passwd varchar(20),Login_set char default 'S');")
+	cur.execute("CREATE TABLE student_details(SId integer primary key autoincrement, Name varchar(30),USN varchar(10) unique,Branch char(5),sem integer,section char,Mobile_Number bigint(10),Email_id varchar(40),passwd varchar(20));")
 	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Anish R A','4NI16CS001','CSE',5,'A',9012345678,'anishra123@gmail.com','anish123');")
-	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Vijith K','4NI16ME003','CSE',5,'B',9123456780,'vijithk123@gmail.com','vijith123');")
-	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('','4NI16','',,'',,'123@gmail.com','123');")
-	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('','4NI16','',,'',,'123@gmail.com','123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Vijith K','4NI16IS001','ISE',5,'A',9123456780,'vijithk123@gmail.com','vijith123');")
+	cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('Shruti M','4NI16CV001','CIVIL',5,'A',9234567801,'shrutim123@gmail.com','shruti123');")
+	#cur.execute("INSERT INTO student_details (Name,USN,Branch,sem,section,Mobile_Number,Email_id,passwd) values('','4NI16','',,'',,'123@gmail.com','123');")
 
 
-	cur.execute("CREATE TABLE teacher_details(TId integer primary key autoincrement default 2001,Tname varchar(30),ID_no varchar(11) unique,dept varchar(30),TMobile_Number bigint(10),TEmail_Id varchar(40),tpwd varchar(20),Login_set char default 'T');")
-	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Satya Nadella','5NI2001CS01','CSE',8012345679,'satyanadela123@gmail.com','satya123');")
-	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Sundar Pichai','5NI1998CS03','CSE',8123456790,'sundarpichai123@gmail.com','sundar123');")
+	cur.execute("CREATE TABLE teacher_details(TId integer primary key autoincrement,Tname varchar(30),ID_no varchar(11) unique,dept varchar(30),TMobile_Number bigint(10),TEmail_Id varchar(40),tpwd varchar(20));")
+	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Sundar Pichai','5NI1998CS01','CSE',8123456790,'sundarpichai123@gmail.com','sundar123');")
+	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Satya Nadella','5NI2001IS01','ISE',8012345679,'satyanadela123@gmail.com','satya123');")
+	cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('Mukesh Ambani','5NI2000EC01','ECE',8234567901,'mukeshambani123@gmail.com','mukesh123');")
+	#cur.execute("INSERT INTO teacher_details (Tname,ID_no,dept,TMobile_Number,TEmail_Id,tpwd) values('','5NI________','',,'123@gmail.com','123');")
 
-	cur.execute("CREATE TABLE student_notes(NId integer primary key autoincrement default 3001, SId integer, Subject varchar(30), Title varchar(30), Notes text ,FOREIGN KEY (SId) REFERENCES student_details(SId));")
-	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(1,'Operating System','Deadlocks','four conditions must hold for there to be a (resource) deadlock: 1. Mutual exclusion condition. 2. Hold-and-wait condition. 3.No-preemption condition. 4.Circular wait condition.');")
-	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(2,'TM','BLAHBLAHBLAHBLAH','BLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAHBLAH');")
+	cur.execute("CREATE TABLE student_notes(NId integer primary key autoincrement, SId integer, Subject varchar(30), Title varchar(30), Notes text ,FOREIGN KEY (SId) REFERENCES student_details(SId));")
+	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(1,'Operating System','Deadlocks','A set of processes is deadlocked if each process in the set is waiting for an event that only another process in the set can cause.');")
+	cur.execute("INSERT INTO student_notes (SId,Subject,Title,Notes) values(2,'DBMS','Views','There are two types of views basically. They are materialized views and virtual views.');")
  
-	cur.execute("CREATE TABLE uploaded_notes(UNId integer primary key autoincrement default 4001, TId integer , Subject varchar(30), Title varchar(30), Notes text, Branch char(5), sem integer, section char, Uploaded_On datetime,FOREIGN KEY (TId) REFERENCES teacher_details(TId));")
-	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(1,'DCN','WiMAX','Fixed and Mobile Services','CSE',5,'A');")
-	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(2,'SS','Assembler Directives','RESW BYTE WORD RESW','CSE',5,'B');")
-
-	cur.execute("CREATE VIEW uplnotelist AS SELECT T.Tname,U.Subject,U.Title,U.Notes,U.Branch,U.sem,U.section,U.Uploaded_On from teacher_details T, uploaded_notes U where T.TId=U.TId;")
+	cur.execute("CREATE TABLE uploaded_notes(UNId integer primary key autoincrement, TId integer , Subject varchar(30), Title varchar(30), Notes text, Branch char(5), sem integer, section char, Uploaded_On datetime,FOREIGN KEY (TId) REFERENCES teacher_details(TId));")
 
 	cur.execute('''CREATE TRIGGER date_time AFTER INSERT ON uploaded_notes  
 					FOR EACH ROW 
@@ -43,7 +41,13 @@ def creation():
 					  UPDATE uploaded_notes set Uploaded_On = datetime('now','localtime') where UNId = new.UNId;
 					END;''')
 
+	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(1,'SS','Assembler Directives','RESW BYTE WORD RESW','CSE',5,'A');")
+	cur.execute("INSERT INTO uploaded_notes (TId,Subject,Title,Notes,Branch,sem,section) values(2,'DCN','WiMAX','Fixed and Mobile Services','ISE',5,'A');")
+	
 
+	cur.execute("CREATE VIEW uplnotelist AS SELECT T.Tname,U.Subject,U.Title,U.Notes,U.Branch,U.sem,U.section,U.Uploaded_On from teacher_details T, uploaded_notes U where T.TId=U.TId;")
+
+	
 	conn.commit()
 	conn.close()
 
